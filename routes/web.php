c58555303185c63a2ff245313b829b73e3f47bd7
Han Huang <vagrant@homestead>
2017-01-02 18:21:55 +0800

    add Voyager & Hesto MultiAuth, fix routing issue of multi-auth

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,17 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::group(['prefix' => 'client'], function () {
+  Route::get('/login', 'ClientAuth\LoginController@showLoginForm');
+  Route::post('/login', 'ClientAuth\LoginController@login');
+  Route::post('/logout', 'ClientAuth\LoginController@logout');
+
+  Route::get('/register', 'ClientAuth\RegisterController@showRegistrationForm');
+  Route::post('/register', 'ClientAuth\RegisterController@register');
+
+  Route::post('/password/email', 'ClientAuth\ForgotPasswordController@sendResetLinkEmail');
+  Route::post('/password/reset', 'ClientAuth\ResetPasswordController@reset');
+  Route::get('/password/reset', 'ClientAuth\ForgotPasswordController@showLinkRequestForm');
+  Route::get('/password/reset/{token}', 'ClientAuth\ResetPasswordController@showResetForm');
+});
