fe6182f775ce423fa36cac82a1a0df97250b34b1
Han Huang <vagrant@homestead>
2017-01-04 23:15:05 +0800

    add mews/captcha and button to reload captcha

--- routes/web.php
+++ routes/web.php
@@ -36,3 +36,50 @@ Route::get('contact', function() {
     return View::make('contact');
 });
 Route::post('contact', 'EnquiryController@index');
+
+Route::any('captcha-test', function(\Illuminate\Http\Request $request)
+{
+    if ($request->method() == 'POST')
+    {
+        $rules = ['captcha' => 'required|captcha'];
+        //$validator = Validator::make(array('captcha'=>$request->input('captcha')), $rules);
+        $data = $request->all();
+        $validator = Validator::make($data, $rules);
+        if ($validator->fails())
+        {
+            echo '<p style="color: #ff0000;">Incorrect!</p>';
+        }
+        else
+        {
+            echo '<p style="color: #00ff30;">Matched :)</p>';
+        }
+    }
+
+    //origin
+    // $form = '<form method="post" action="captcha-test">';
+    // $form .= '<input type="hidden" name="_token" value="' . csrf_token() . '">';
+    // $form .= '<p>' . captcha_img() . '</p>';
+    // $form .= '<p><input type="text" name="captcha"></p>';
+    // $form .= '<p><button type="submit" name="check">Check</button></p>';
+    // $form .= '</form>';
+    // return $form;
+
+    //add button to reload captcha
+    $form = '<!DOCTYPE html><html><script>';
+    $form .= 'function reload_captcha(){';
+    $form .= 'var date = new Date();';
+    $form .= 'var obj = document.getElementsByTagName("img")[0];';
+    $form .= 'obj.src = "'.url('captcha/default?').'" + date.getTime();';
+    $form .= 'console.log(obj.src);';
+    $form .= 'return false;}</script><body>';
+
+    $form .= '<form method="post" action="captcha-test">';
+    $form .= '<input type="hidden" name="_token" value="' . csrf_token() . '">';
+    $form .= '<p>' . captcha_img() . '<button type="button" name="reload" onclick="return reload_captcha();">Reload</button></p>';
+    $form .= '<p><input type="text" name="captcha"></p>';
+    $form .= '<p><button type="submit" name="check">Check</button></p>';
+
+    $form .= '</form></body></html>';
+    return $form;
+
+});
