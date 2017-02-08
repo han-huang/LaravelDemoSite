2a3fb0871e239c9e255387fbfd3d735883eb01b0
Han Huang <vagrant@homestead>
2017-02-09 00:53:04 +0800

    add NewsController.php and modify app/NewsCategory.php, app/NewsPost.php, patches/web.php and patches/routes.web.php.patch

--- app/NewsPost.php
+++ app/NewsPost.php
@@ -5,6 +5,7 @@ namespace App;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Auth;
+use Log;
 
 class NewsPost extends Model
 {
@@ -39,6 +40,112 @@ class NewsPost extends Model
     public function scopePublished(Builder $query)
     {
         return $query->where('status', '=', static::PUBLISHED);
+    }
+
+    /**
+     * Scope a query to only active scopes.
+     *
+     * @param \Illuminate\Database\Eloquent\Builder $query
+     *
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function scopeActive(Builder $query)
+    {
+        // Log::info('active '.__FILE__." ".__FUNCTION__." ".__LINE__);
+        return $query->where('active', 1);
+    }
+
+    /**
+     * Scope a query to find id.
+     *
+     * @param \Illuminate\Database\Eloquent\Builder $query
+     *
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function scopeFindid(Builder $query, $id)
+    {
+        // Log::info('active '.__FILE__." ".__FUNCTION__." ".__LINE__);
+        return $query->where('id', '=', $id);
+    }
+
+    /**
+     * Scope a query to select brief index.
+     *
+     * @param \Illuminate\Database\Eloquent\Builder $query
+     *
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function scopeSelectbrief(Builder $query)
+    {
+        // Log::info('select brief index '.__FILE__." ".__FUNCTION__." ".__LINE__);
+        return $query->select(['id', 'news_category_id', 'title', 'updated_at']);
+    }
+
+    /**
+     * Scope a query to order by updated_at desc.
+     *
+     * @param \Illuminate\Database\Eloquent\Builder $query
+     *
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function scopeUpdatedtimedesc(Builder $query)
+    {
+        // Log::info('order by updated_at desc '.__FILE__." ".__FUNCTION__." ".__LINE__);
+        return $query->orderBy('updated_at', 'desc');
+    }
+
+    /**
+     * Scope a query to get limited rows.
+     *
+     * @param \Illuminate\Database\Eloquent\Builder $query
+     *
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function scopeGetlimit(Builder $query, $offset = 0, $limit = 10)
+    {
+        // Log::info('get limited rows '.__FILE__." ".__FUNCTION__." ".__LINE__);
+        return $query->offset($offset)->limit($limit);
+    }
+
+    /**
+     * Scope a query to get news page.
+     *
+     * @param \Illuminate\Database\Eloquent\Builder $query
+     *
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function scopeNewspage(Builder $query, $offset = 0, $limit = 10)
+    {
+        // Log::info('get news page '.__FILE__." ".__FUNCTION__." ".__LINE__);
+        return $query->selectbrief()->active()->published()
+		           ->updatedtimedesc()->getlimit($offset, $limit);
+    }
+
+    /**
+     * Scope a query to get news article.
+     *
+     * @param \Illuminate\Database\Eloquent\Builder $query
+     * @param $id
+     *
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function scopeNewsarticle(Builder $query, $id)
+    {
+        // Log::info('get news article, $id: '.$id." ".__FILE__." ".__FUNCTION__." ".__LINE__);
+        return $query->findid($id)->active()->published();
+    }
+
+    /**
+     * Scope a query to count news article.
+     *
+     * @param \Illuminate\Database\Eloquent\Builder $query
+     *
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function scopeNewscount(Builder $query)
+    {
+        // Log::info('count news article '.__FILE__." ".__FUNCTION__." ".__LINE__);
+        return $query->active()->published()->count();
     }
 
     /**
--- patches/web.php
+++ patches/web.php
@@ -86,6 +86,14 @@ Route::any('captcha-test', function(\Illuminate\Http\Request $request)
 
 Route::get('/', 'IndexController@getIndexView');
 
+Route::get('/news_content', function () {
+    return view('site.news.temp.news_content_static');
+});
+
+Route::get('/news_index', function () {
+    return view('site.news.temp.news_index_static');
+});
+
 Route::group(['prefix' => 'admin'], function () {
 
     Voyager::routes();
@@ -124,4 +132,36 @@ Route::group(['prefix' => 'admin'], function () {
             ]]);
         });
     });
+});
+
+Route::get('/news', ['uses' => 'NewsController@newsIndex', 'as' => 'news']);
+
+Route::group([
+    'as'     => 'news.',
+    'prefix' => 'news',
+], function () {
+    try {
+        foreach (\App\NewsCategory::all() as $categories) {
+            Route::resource($categories->str, 'NewsController', ['only' => [
+                'index', 'show'
+            ],'names' => [
+                'show' => $categories->str.'.page'
+            ]]);
+        }
+    } catch (\InvalidArgumentException $e) {
+        throw new \InvalidArgumentException("Custom routes hasn't been configured because: ".$e->getMessage(), 1);
+    } catch (\Exception $e) {
+        // do nothing, might just be because table not yet migrated.
+    }
+
+    // News Article
+    Route::group([
+        'as'     => 'article.',
+        'prefix' => 'article',
+    ], function () {
+        Route::get('{id}', ['uses' => 'NewsController@newsArticle', 'as' => 'content']);
+    });
+
+    // News Page
+    Route::get('page/{id}', ['uses' => 'NewsController@newsPage', 'as' => 'page']);
 });
