4eeae86a3d9f1b324aa95094b1a4310a350e0bf3
Han Huang <vagrant@homestead>
2017-02-10 23:21:10 +0800

    add routes/news.php and modify related files

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -41,7 +41,26 @@ class RouteServiceProvider extends ServiceProvider
 
         $this->mapClientRoutes();
 
+        $this->mapNewsRoutes();
+
         //
+    }
+
+    /**
+     * Define the "news" routes for the application.
+     *
+     * These routes all receive session state, CSRF protection, etc.
+     *
+     * @return void
+     */
+    protected function mapNewsRoutes()
+    {
+        Route::group([
+            'middleware' => ['web'],
+            'namespace' => $this->namespace,
+        ], function ($router) {
+            require base_path('routes/news.php');
+        });
     }
 
     /**
