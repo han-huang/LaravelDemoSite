bad4961c7e1f2742c6c9e27a8be56f9bda31cd5a
Han Huang <vagrant@homestead>
2017-02-17 20:21:51 +0800

    add ajax tutorial

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,91 +1,106 @@
 <!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<html lang="en-US">
+<head>
+    <title>Laravel Ajax CRUD Example</title>
 
-        <title>Laravel</title>
+    <!-- Load Bootstrap CSS
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">
+    -->
+    <!-- Bootstrap core CSS -->
+    <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet" type="text/css">
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <link href="{{ asset('css/ie10-viewport-bug-workaround.css') }}" rel="stylesheet" type="text/css">
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+</head>
+<body>
+    <div class="container-narrow">
+        <h2>Laravel Ajax ToDo App</h2>
+        <button id="btn-add" name="btn-add" class="btn btn-primary btn-xs">Add New Task</button>
+        <div>
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
+            <!-- Table-to-load-the-data Part -->
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Task</th>
+                        <th>Description</th>
+                        <th>Date Created</th>
+                        <th>Actions</th>
+                    </tr>
+                </thead>
+                <tbody id="tasks-list" name="tasks-list">
+                    @foreach ($tasks as $task)
+                    <tr id="task{{$task->id}}">
+                        <td>{{$task->id}}</td>
+                        <td>{{$task->task}}</td>
+                        <td>{{$task->description}}</td>
+                        <td>{{$task->created_at}}</td>
+                        <td>
+                            <button class="btn btn-warning btn-xs btn-detail open-modal" value="{{$task->id}}">Edit</button>
+                            <button class="btn btn-danger btn-xs btn-delete delete-task" value="{{$task->id}}">Delete</button>
+                        </td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
+            <!-- End of Table-to-load-the-data Part -->
+            <!-- Modal (Pop up when detail button clicked) -->
+            <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">×</span></button>
+                            <h4 class="modal-title" id="myModalLabel">Task Editor</h4>
+                        </div>
+                        <div class="modal-body">
+                            <form id="frmTasks" name="frmTasks" class="form-horizontal" novalidate="">
+                                {{ csrf_field() }}
+                                <div class="form-group error">
+                                    <label for="inputTask" class="col-sm-3 control-label">Task</label>
+                                    <div class="col-sm-9">
+                                        <input type="text" class="form-control has-error" id="task" name="task" placeholder="Task" value="">
+                                    </div>
+                                </div>
 
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    <a href="{{ url('/login') }}">Login</a>
-                    <a href="{{ url('/register') }}">Register</a>
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                                <div class="form-group">
+                                    <label for="inputEmail3" class="col-sm-3 control-label">Description</label>
+                                    <div class="col-sm-9">
+                                        <input type="text" class="form-control" id="description" name="description" placeholder="Description" value="">
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-primary" id="btn-save" value="add">Save changes</button>
+                            <input type="hidden" id="task_id" name="task_id" value="0">
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
-    </body>
+    </div>
+    <!-- <meta name="_token" content="{{ csrf_token() }}" /> -->
+	<meta name="csrf-token" content="{{ csrf_token() }}">
+    <!--
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+    <script src="{{asset('js/ajax-crud.js')}}"></script>
+    -->
+
+    <!-- Bootstrap core JavaScript -->
+    <script type="text/javascript" src="{{ asset('js/jquery-3.1.0.min.js') }}"></script>
+    <script>window.jQuery || document.write('<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"><\/script>')</script>
+    <script type="text/javascript" src="{{ asset('js/bootstrap.min.js') }}"></script>
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script type="text/javascript" src="{{ asset('js/ie10-viewport-bug-workaround.js') }}"></script>
+    <script src="{{asset('js/ajax-crud.js')}}"></script>
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,40 @@
 |
 */
 
+use Illuminate\Http\Request;
+
 Route::get('/', function () {
-    return view('welcome');
+    // return view('welcome');
+
+    $tasks = App\Task::all();
+    return View::make('welcome')->with('tasks',$tasks);
+});
+
+Route::get('/tasks/{task_id?}',function($task_id){
+    $task = App\Task::find($task_id);
+
+    return response()->json($task);
+});
+
+Route::post('/tasks',function(Request $request){
+    $task = App\Task::create($request->all());
+
+    return response()->json($task);
+});
+
+Route::put('/tasks/{task_id?}',function(Request $request,$task_id){
+    $task = App\Task::find($task_id);
+
+    $task->task = $request->task;
+    $task->description = $request->description;
+
+    $task->save();
+
+    return response()->json($task);
+});
+
+Route::delete('/tasks/{task_id?}',function($task_id){
+    $task = App\Task::destroy($task_id);
+
+    return response()->json($task);
 });
