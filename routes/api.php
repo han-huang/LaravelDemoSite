354ffe496e000731a1714a4183364512dcf9b708
Han Huang <vagrant@homestead>
2017-02-24 01:15:26 +0800

    install "tymon/jwt-auth", "dingo/api" add api token to users table modify api.php for Dingo Router
    Postman :
    
    usage of api_token :
    GET http://domain.app:8029/api/user?api_token=Ia4fPbCEBf3OvNzq81oBi9O4DtKZbwQlKc7CKWhCGXdnje3bnY4Iyb9Y9DzN
    
    Post http://domain.app:8029/api/auth/login
    Params :
    'email' : 'admin@admin.com'
    'password' : 'password'
    
    {
      "status": "ok",
      "token": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOjEsImlzcyI6Imh0dHA6XC9cL2RvbWFpbi5hcHA6ODAyOVwvYXBpXC9hdXRoXC9sb2dpbiIsImlhdCI6MTQ4Nzg2NTczMSwiZXhwIjoxNDg3ODY5MzMxLCJuYmYiOjE0ODc4NjU3MzEsImp0aSI6IjIxOWMzZmU0OGEzNjEzYjFjZmU0MWMwMWM2YzZjNDZkIn0.9km7lAPInTRmFVM9bNTWzsCuIgvqnwjCbGiMKYVGQj4"
    }
    
    Get http://domain.app:8029/api/protected
    Header :
    Authorization: Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOjEsImlzcyI6Imh0dHA6XC9cL2RvbWFpbi5hcHA6ODAyOVwvYXBpXC9hdXRoXC9sb2dpbiIsImlhdCI6MTQ4Nzg2NjUxOCwiZXhwIjoxNDg3ODcwMTE4LCJuYmYiOjE0ODc4NjY1MTgsImp0aSI6IjVjOTA3MjgxNmIzY2NlZTE3MjhlOWRkZDdiMTgzNGUxIn0.VVgBOk6cPNBci--tPjLNSRKrvUo5nC7N8vMJ_ToCWqI
    
    {"message":"Access to this item is only for authenticated user. Provide a token in your request!"}
    
    Reference
    
    https://www.sitepoint.com/how-to-build-an-api-only-jwt-powered-laravel-app/
    https://github.com/francescomalatesta/laravel-api-boilerplate-jwt
    
    https://github.com/tymondesigns/jwt-auth
    https://github.com/dingo/api/
    
    https://arjunphp.com/create-rest-laravel-framework/
    https://laracasts.com/discuss/channels/laravel/53-api-routes-auth-middleware-confusion
    https://gistlog.co/JacobBennett/090369fbab0b31130b51

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
+use Dingo\Api\Routing\Router;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,8 +14,72 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:api');
+// Route::get('/user', function (Request $request) {
+    // return $request->user();
+// })->middleware('auth:api')->name('user');
 
-Route::get('article/{id}', ['uses' => 'NewsController@newsArticleApi', 'as' => 'article']);
+// Route::get('article/{id}', ['uses' => 'NewsController@newsArticleApi', 'as' => 'article']);
+
+// Route::group(['prefix' => 'auth', 'as' => 'auth.'], function () {
+    // Route::post('signup', '\App\\Api\\V1\\Controllers\\SignUpController@signUp')->name('signup');
+    // Route::post('login', '\App\\Api\\V1\\Controllers\\LoginController@login')->name('login');
+
+    // Route::post('recovery', '\App\\Api\\V1\\Controllers\\ForgotPasswordController@sendResetEmail')->name('recovery');
+    // Route::post('reset', '\App\\Api\\V1\\Controllers\\ResetPasswordController@resetPassword')->name('reset');
+// });
+
+// Route::group(['middleware' => 'jwt.auth'], function () {
+    // Route::get('protected', function() {
+        // return response()->json([
+            // 'message' => 'Access to this item is only for authenticated user. Provide a token in your request!'
+        // ]);
+    // })->name('protected');
+
+    // Route::get('refresh', [
+        // 'middleware' => 'jwt.refresh',
+        // function() {
+            // return response()->json([
+                // 'message' => 'By accessing this endpoint, you can refresh your access token at each request. Check out this response headers!'
+            // ]);
+        // }
+    // ])->name('refresh');
+// });
+
+$api = app('Dingo\Api\Routing\Router');
+$api->version('v1', function ($api) {
+
+    $api->get('test', function () {
+        return 'It is ok';
+    });
+
+    $api->get('/user', ['middleware' => 'auth:api', function (Request $request) {
+        return $request->user();
+    }]);
+
+    $api->get('article/{id}', ['uses' => 'App\\Http\\Controllers\\NewsController@newsArticleApi', 'as' => 'article']);
+
+    $api->group(['prefix' => 'auth', 'as' => 'auth.'], function(Router $api) {
+        $api->post('signup', 'App\\Api\\V1\\Controllers\\SignUpController@signUp');
+        $api->post('login', 'App\\Api\\V1\\Controllers\\LoginController@login');
+
+        $api->post('recovery', 'App\\Api\\V1\\Controllers\\ForgotPasswordController@sendResetEmail');
+        $api->post('reset', 'App\\Api\\V1\\Controllers\\ResetPasswordController@resetPassword');
+    });
+
+    $api->group(['middleware' => 'jwt.auth'], function(Router $api) {
+        $api->get('protected', function() {
+            return response()->json([
+                'message' => 'Access to this item is only for authenticated user. Provide a token in your request!'
+            ]);
+        });
+
+        $api->get('refresh', [
+            'middleware' => 'jwt.refresh',
+            function() {
+                return response()->json([
+                    'message' => 'By accessing this endpoint, you can refresh your access token at each request. Check out this response headers!'
+                ]);
+            }
+        ]);
+    });
+});
