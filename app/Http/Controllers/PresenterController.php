8960ce419569b69b57447aede0adcc98fa63b33a
Han Huang <vagrant@LaravelDemoSite>
2017-03-26 16:59:55 +0000

    resize images of book

--- app/Http/Controllers/PresenterController.php
+++ app/Http/Controllers/PresenterController.php
@@ -17,4 +17,26 @@ class PresenterController extends Controller
     {
         return mb_strlen($str, 'UTF-8') > $length ? mb_substr($str, 0, $length, 'UTF-8')." ..." : $str;
     }
+
+    /**
+     * replace path to medium image.
+     *
+     * @param  string  $path
+     * @return string
+     */
+    public function mediumimg($path)
+    {
+        return str_replace('.jpg', '-medium.jpg', $path);
+    }
+
+    /**
+     * replace path to small image.
+     *
+     * @param  string  $path
+     * @return string
+     */
+    public function smallimg($path)
+    {
+        return str_replace('.jpg', '-small.jpg', $path);
+    }
 }
