c264c32b7e63428dab2bdb01205051f65536359d
Han Huang <vagrant@LaravelDemoSite>
2017-03-28 13:27:12 +0000

    install 'gloudemans/shoppingcart' add column 'book_quanity' to book_order table add ClientsTableSeeder.php and ReceiversTableSeeder.php
    https://github.com/Crinsane/LaravelShoppingcart
    
    Laravel: extra field sync with array
    http://stackoverflow.com/questions/28903692/laravel-extra-field-sync-with-array
    
    Many to many relationship with extra field
    https://laravel.io/forum/04-12-2015-many-to-many-relationship-with-extra-field
    
    https://laravel.com/docs/5.3/eloquent-relationships
    http://d.laravel-china.org/docs/5.3/eloquent-relationships
    http://laravelacademy.org/post/6191.html
    
    vagrant@homestead:~/Code/Laravel/public/bookstore_temp_install$ artisan tinker
    >>> use App\Order;$order=Order::find(1);$data=[2=>['book_quanity'=> 3], 3=>['book_quanity'=>5]];$order->books()->sync($data);
    => [
         "attached" => [
           2,
           3,
         ],
         "detached" => [],
         "updated" => [],
       ]
    
    mysql> select * from book_order;
    +----------+---------+--------------+
    | order_id | book_id | book_quanity |
    +----------+---------+--------------+
    |        1 |       2 |            3 |
    |        1 |       3 |            5 |
    +----------+---------+--------------+
    2 rows in set (0.00 sec)

---
+++ database/seeds/ReceiversTableSeeder.php
@@ -0,0 +1,67 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Receiver;
+use App\Client;
+
+class ReceiversTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        if (Receiver::count() == 0) {
+
+			$client = Client::where('name', 'Alice')->firstOrFail();
+            Receiver::create([
+                'name'           => 'Alice',
+                'client_id'      => $client->id,
+                'phone'          => '0999123999',
+                'email'          => 'alice@example.jp',
+                'addr_city'      => '台南市',
+                'addr_area'      => '安南區',
+                'addr_street'    => '安中路１段10號',
+                'zip_code'       => 709,
+            ]);
+
+			$client = Client::where('name', 'Bob')->firstOrFail();
+            Receiver::create([
+                'name'           => 'Bob',
+                'client_id'      => $client->id,
+                'phone'          => '0999123888',
+                'email'          => 'bob@example.jp',
+                'addr_city'      => '台南市',
+                'addr_area'      => '安南區',
+                'addr_street'    => '同安路10號',
+                'zip_code'       => 709,
+            ]);
+
+			$client = Client::where('name', 'Carol')->firstOrFail();
+            Receiver::create([
+                'name'           => 'Carol',
+                'client_id'      => $client->id,
+                'phone'          => '0999123777',
+                'email'          => 'carol@example.jp',
+                'addr_city'      => '台南市',
+                'addr_area'      => '安南區',
+                'addr_street'    => '海佃路１段10號',
+                'zip_code'       => 709,
+            ]);
+
+			$client = Client::where('name', 'Wasbook')->firstOrFail();
+            Receiver::create([
+                'name'           => 'Wasbook',
+                'client_id'      => $client->id,
+                'phone'          => '0999123666',
+                'email'          => 'wasbook@example.jp',
+                'addr_city'      => '台南市',
+                'addr_area'      => '安南區',
+                'addr_street'    => '國安街10號',
+                'zip_code'       => 709,
+            ]);
+        }
+    }
+}
